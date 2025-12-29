e065157fc8e9f6d53333250091095828c394d1af
TheJolt3 <brunoax05@gmail.com>
2025-12-29 07:56:36 -0300

    Update app.js
    Acertando e errando o número secreto

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do Número Secreto!');
-let numeroSecreto = 4;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
@@ -7,5 +7,9 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (numeroSecreto == chute) {
     console.log(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
 } else{
-    alert('Você errou :(');
+    if(chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`)
+    }
 }
