1c19ad376694e89ae287e2176b4004567739c829
TheJolt3 <brunoax05@gmail.com>
2025-12-30 07:27:13 -0300

    Update app.js
    Utilizando o "while (enquanto)"

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 alert('Boas vindas ao jogo do Número Secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute 
 
-// se chute for igual ao número secreto 
-if (numeroSecreto == chute) {
-    console.log(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
-} else{
-    if(chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
-    } else {
-        alert(`O número secreto é maior que ${chute}`)
+// enquanto o chute não for igual ao n.s
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto 
+    if (chute > numeroSecreto) {
+        console.log(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
+    } else{
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
     }
 }
