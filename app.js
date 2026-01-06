f26052ce02873b9a4f0bb656f7041f2ca4fc408d
TheJolt3 <brunoax05@gmail.com>
2026-01-06 07:36:54 -0300

    Update app.js
    Colocando tentativas e arrumando falhas anteriores

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 alert('Boas vindas ao jogo do Número Secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute 
+let chute;
+let tentativas = 1;
 
 // enquanto o chute não for igual ao n.s
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     // se chute for igual ao número secreto 
-    if (chute > numeroSecreto) {
-        console.log(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else{
         if(chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
+        // tentativas = tentativass + 1;
+        tentativas++;
     }
 }
