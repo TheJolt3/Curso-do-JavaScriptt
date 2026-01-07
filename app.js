c985f7fe36a252fdadf88adc7ad6008585fdd32b
TheJolt3 <brunoax05@gmail.com>
2026-01-07 07:45:19 -0300

    Update app.js
    Usando o "Operador ternário" para simplificar e deixar o código mais legível

--- app.js
+++ app.js
@@ -21,8 +21,10 @@ while (chute != numeroSecreto) {
     }
 }
 
-if (tentativas > 1) {
-    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-} else {
-    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+//if (tentativas > 1) {
+//    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// } else {
+//    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com $///{tentativas} tentativas.`);
+// }
